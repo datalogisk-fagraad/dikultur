a3b414171286dc8d328e260b17003cf597ea0cd3
Víðir Valberg Guðmundsson <valberg@orn.li>
2014-09-29 16:15:21 +0200

    Some URL cleaning

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = patterns('',
     url(r'^calendar$', views.CalendarFeed.as_view(), name='calendar'),
     url(r'^create$', views.EventCreate.as_view(), name='create'),
-    url(r'^(?P<slug>\S+)/update$', views.EventUpdate.as_view(), name='update'),
-    url(r'^(?P<slug>\S+)$', views.EventDetail.as_view(), name='detail'),
+
+    url(r'^detail/(?P<slug>\S+)/update$', views.EventUpdate.as_view(), name='update'),
+    url(r'^detail/(?P<slug>\S+)$', views.EventDetail.as_view(), name='detail'),
+
     url(r'^$', views.EventList.as_view(), name='list'),
 )
--- apps/resources/urls.py
+++ apps/resources/urls.py
@@ -5,10 +5,14 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^create/$', views.ResourceCreate.as_view(), name='create'),
-    url(r'^(?P<slug>\S+)/remove_upvote$', views.resource_remove_upvote, name='remove_upvote'),
-    url(r'^(?P<slug>\S+)/upvote$', views.resource_upvote, name='upvote'),
-    url(r'^(?P<slug>\S+)/update$', views.ResourceUpdate.as_view(), name='update'),
-    url(r'^(?P<slug>\S+)/update/delete-file$', views.resource_file_delete, name='delete-file'),
-    url(r'^(?P<slug>\S+)$', views.ResourceDetail.as_view(), name='detail'),
+
+    url(r'^detail/(?P<slug>\S+)/remove_upvote$', views.resource_remove_upvote, name='remove_upvote'),
+    url(r'^detail/(?P<slug>\S+)/upvote$', views.resource_upvote, name='upvote'),
+
+    url(r'^detail/(?P<slug>\S+)/update/delete_file$', views.resource_file_delete, name='delete-file'),
+    url(r'^detail/(?P<slug>\S+)/update$', views.ResourceUpdate.as_view(), name='update'),
+
+    url(r'^detail/(?P<slug>\S+)$', views.ResourceDetail.as_view(), name='detail'),
+
     url(r'^$', views.ResourceList.as_view(), name='list'),
 )
