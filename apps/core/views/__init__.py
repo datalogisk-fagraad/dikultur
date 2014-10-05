b352c7bff2ba3f090eebbadf17d0ce17f0371075
Víðir Valberg Guðmundsson <valberg@orn.li>
2014-10-05 17:16:05 +0200

    Procrastination-commit! Adding events to frontpage.

--- apps/core/views/__init__.py
+++ apps/core/views/__init__.py
@@ -4,9 +4,17 @@ from django.views.generic import TemplateView
 
 from allauth.account.views import LogoutView
 
+from ...events.models import Event
+
 
 class FrontPage(TemplateView):
     template_name = 'core/frontpage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['events'] = Event.objects.filter(
+            public=True).order_by('datetime')[:5]
+        return context
 
 
 class ProfileView(TemplateView):
