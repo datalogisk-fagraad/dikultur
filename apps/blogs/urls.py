348d0d3e9a56f83b4e5a602aaf83bd5a162f354e
sebastian <sebastian@skjoenberg.com>
2014-10-17 11:08:32 +0200

    Changed the structure a bit.

--- apps/blogs/urls.py
+++ apps/blogs/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
-
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = patterns('',
+    url(r'^(?P<slug>all)$', views.BlogIndex.as_view(), name='all'),
+    url(r'^(?P<blog_slug>\S+)/(?P<slug>\S+)/update$', views.PostUpdate.as_view(), name='post-update'),
+    url(r'^(?P<blog_slug>\S+)/(?P<slug>\S+)$', views.PostDetail.as_view(), name='post-detail'),
     url(r'^(?P<slug>\S+)/create$', views.PostCreate.as_view(), name='create'),
-
-    url(r'^all$', views.PostList.as_view(), name='list'),
-    url(r'^(?P<slug>\S+)$', views.PostList.as_view(), name='list'),
-    url(r'^$', views.BlogList.as_view(), name='bloglist'),
+    url(r'^(?P<slug>\S+)/$', views.BlogDetail.as_view(), name='blog-detail'),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('blogs:all', kwargs={'slug': 'all'})), name='root'),
 )
