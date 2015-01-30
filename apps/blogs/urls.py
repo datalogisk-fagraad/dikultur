511fa422bfb528d466b46db39f3bb7c220147f05
sebastian <sebastian@skjoenberg.com>
2015-01-30 18:26:58 +0100

    Changed the name of create to post-create. I hope i didn't break something

--- apps/blogs/urls.py
+++ apps/blogs/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^(?P<slug>all)$', views.BlogIndex.as_view(), name='all'),
-    url(r'^(?P<slug>\S+)/create$', views.PostCreate.as_view(), name='create'),
+    url(r'^(?P<slug>\S+)/create$', views.PostCreate.as_view(), name='post-create'),
     url(r'^(?P<blog_slug>\S+)/(?P<slug>\S+)/update$', views.PostUpdate.as_view(), name='post-update'),
     url(r'^(?P<blog_slug>\S+)/(?P<slug>\S+)$', views.PostDetail.as_view(), name='post-detail'),
     url(r'^(?P<slug>\S+)/$', views.BlogDetail.as_view(), name='blog-detail'),
