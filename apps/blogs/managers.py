8c4107fd35f23ba6f9eff40f1113263ca6648df9
sebastian <sebastian@skjoenberg.com>
2015-02-13 00:22:47 +0100

    tilfreds?

--- apps/blogs/managers.py
+++ apps/blogs/managers.py
@@ -5,5 +5,8 @@ class PostQuerySet(models.QuerySet):
     def deleted(self):
         return self.filter(is_deleted=True)
 
+    def live(self):
+        return self.filter(is_deleted=False)
+
     def public(self):
         return self.filter(public=True, is_deleted=False)
